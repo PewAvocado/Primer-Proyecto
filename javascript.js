f31e33da4c0a4509ddaa40f2ad40dcba68b93487
Alvaro Ruiz <ar102rc@gmail.com>
2025-11-13 10:28:27 -0500

    Aca practique las 3 funciones (tipos) pero debo hacerlo mas a diario

--- javascript.js
+++ javascript.js
@@ -7,3 +7,24 @@ const apellido = "Ruiz";
 const año = 2025;
 
 let alive = true;
+
+function saludo (nombre){
+    console.log("Hola " + nombre + ", espero que tengas un buen dia.");
+}
+saludo("Ricardo");
+
+//FUNCTION REGULAR
+function alimentos(verdura){
+    console.log("La " + verdura + " es muy rica y nutritiva");
+}
+alimentos ("Espinaca");
+
+//ARROW FUNCTIONS
+const chatarra = (fritura) => console.log("La " + fritura + " es insalubre en exceso.");
+chatarra("hamburguesa");
+
+//FUNCTION EXPRESSIONS
+let division = function (a, b){
+    console.log(a/b);
+};
+division(30, 5);
